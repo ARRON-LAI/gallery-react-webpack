b5f54c82b16d0a17c9483fe58b64a9b62e62c579
Arron <arron.laihongwei@gmail.com>
2018-01-28 22:52:38 +0800

    image section half way -- render DOM from imageData

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,17 +8,36 @@ let imageData = require('../data/imageData.json');
 imageData = (function genImageData(imgs) {
   for(let i = 0; i < imgs.length; ++i) {
     const fn = imgs[i].fileName;
-    imgs[i].imageUrl = require('../images/' + fn);
+    imgs[i].imageURL = require('../images/' + fn);
   }
   return imgs;
 })(imageData);
 
+class ImageFigure extends React.Component {
+  render() {
+    return (
+      <figure className="img-figure">
+        <img src={this.props.data.imageURL}
+            alt={this.props.data.title} />
+        <figcaption>
+          <h2 className="img-title">
+            {this.props.data.title}
+          </h2>
+        </figcaption>
+      </figure>
+    )
+  }
+}
 class AppComponent extends React.Component {
   render() {
+    let imageFigures = [];
+    imageData.forEach(value => {
+      imageFigures.push(<ImageFigure data={value}/>)
+    })
     return (
       <section className="stage">
         <section className="img-sec">
-
+          {imageFigures}
         </section>
         <nav className="controller"></nav>
       </section>
